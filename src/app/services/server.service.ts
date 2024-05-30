331fd6aef9913100ac9ad511ed09a52f767f4598
Gonzalo <cacalo219@hotmail.com>
2024-05-30 16:27:18 -0300

    Desconexión del back manejada

--- src/app/services/server.service.ts
+++ src/app/services/server.service.ts
@@ -3,6 +3,7 @@ import { io } from 'socket.io-client';
 import { environment } from '../../environments/environment';
 import { Observable, Subject } from 'rxjs';
 import { SalaJuego } from '../interfaces/sala';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,7 @@ export class ServerService {
 
   server = io(environment.SERVER_URL,{ autoConnect: false });
   actualizacionesDeSala$ = new Subject<SalaJuego>();
+  router = inject(Router);
 
   constructor() {
     // this.server.onAny((args)=> console.log("Comunicación recibida",args));
@@ -20,6 +22,10 @@ export class ServerService {
     this.server.on("sala",(sala:SalaJuego)=>{
       // console.log("Update sala",sala);
       this.actualizacionesDeSala$.next(sala)
+    })
+    this.server.on("disconnect", ()=> {
+      this.router.navigate(["/"]);
+      //console.log("Desconxión del back")
     })
     this.server.connect();
    }
